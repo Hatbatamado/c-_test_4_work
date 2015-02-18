48098e88f374bf237bccf5dab6da4a13d651b354
Nakia <nakiaaresz@gmail.com>
2015-02-18 21:16:23 +0100

    +komment és fix 1-eshez

--- Src/elso.cs
+++ Src/elso.cs
@@ -10,25 +10,24 @@ namespace test
     {
         static public void Feladat()
         {
-            int result;
-
+            int result; //megadott szám konvertált értéke
             do
             {
                 Console.Clear();
                 Console.Write("Adjon meg egy pozitív egész számot: ");
-            }
-            while (!(int.TryParse(Console.ReadLine(), out result)));
+            }//csak akkor fusson tovább, ha pozitív és ha sikerült konvertálni
+            while (!int.TryParse(Console.ReadLine(), out result) || result < 0);
 
-            if (result >= 2)
+            if (result >= 2) //2-től vannak a prím számok
             {
-                int gyok = (int)Math.Sqrt(result);
+                int gyok = (int)Math.Sqrt(result); //sorozat gyökéig kell nézni
                 int db = 0;
 
-                for (int i = 1; i <= gyok; i++)
+                for (int i = 1; i <= gyok; i++) //osztó db sztámok keresése
                     if (result % i == 0)
                         db++;
 
-                if (db == 1)
+                if (db == 1) //osztó db szám alapján prím-e
                     Console.Write("A megadott szám prím szám");
                 else
                     Console.Write("A megadott szám nem prím szám");
